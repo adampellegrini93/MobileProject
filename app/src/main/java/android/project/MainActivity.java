af391715f39ddb0be6fce3ea70af0fe888c3adee
jacques <farris.jacques@gmail.com>
2018-09-27 15:36:03 -0400

    updated login screen and sign up screen

--- app/src/main/java/android/project/MainActivity.java
+++ app/src/main/java/android/project/MainActivity.java
@@ -1,13 +1,53 @@
 package android.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SIGN_UP = 0;
+
+    TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUp = (TextView)findViewById(R.id.signUp);
+
+        final String createAccount = signUp.getText().toString();
+
+        SpannableString txt = new SpannableString(createAccount);
+        ClickableSpan clickTxt = new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+
+        txt.setSpan(clickTxt,32,38, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        signUp.setText(txt);
+        signUp.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signUpIntent = new Intent(getApplicationContext(),createAccount.class);
+                startActivityForResult(signUpIntent, SIGN_UP);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/android/project/createAccount.java
@@ -0,0 +1,19 @@
+package android.project;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class createAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+    }
+
+}
