05e7e1e8e787f3047c77abd95c86e8eaa24e6658
Adam <apelleg2@students.kennesaw.edu>
2018-10-03 13:39:06 -0400

    upload image functionality/alert dialogs

--- app/src/main/java/android/project/HomePage.java
+++ app/src/main/java/android/project/HomePage.java
@@ -34,7 +34,6 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener{
 
     private Toolbar myTool;
     private CardView cardView,cardView2,cardView3;
-    private ImageButton uploadImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,87 +51,8 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener{
 
         myTool = (Toolbar) findViewById(R.id.mytoolbar);
         setSupportActionBar(myTool);
-
-        //allows user to upload photo from phone gallery
-        uploadImage = (ImageButton)findViewById(R.id.homepagePhoto);
-        uploadImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectImage();
-            }
-        });
     }
 
-    //creates pop up box with photo upload options
-    protected void selectImage(){
-        final CharSequence[] options = {"Take Photo","Choose from Gallery","Cancel"};
-        AlertDialog.Builder addPhoto = new AlertDialog.Builder(HomePage.this);
-        addPhoto.setTitle("Add Photo:");
-        addPhoto.setItems(options, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if(options[which].equals("Take Photo")){
-                    activeTakePhoto();
-                }
-                else if(options[which].equals("Choose from Gallery")) {
-                    Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(intent,2);
-                }
-                else if(options[which].equals("Cancel")) {
-                    dialog.dismiss();
-                }
-            }
-        });
-        addPhoto.show();
-    }
-
-    //checks if permission to use phones camera and storage is already allowed
-    private void activeTakePhoto() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 110);
-        } else {
-            takePicture();
-        }
-    }
-
-    //launches functionality for taking/uploading a new photo
-    public void takePicture() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 1);
-    }
-
-    //asks user if app can access the phones camera and storage if not allowed
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == 110) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                takePicture();
-            }
-        }}
-
-    //functionality for taking/uploading a new photo
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            if(requestCode == 1){ //if wanting to take a new picture
-                Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-                uploadImage.setImageBitmap(Bitmap.createScaledBitmap(bitmap,uploadImage.getWidth(),uploadImage.getHeight(),false));
-
-            } else if (requestCode == 2) { //if wanting to upload image from phone storage
-                Uri selectedImage = data.getData();
-                String[] filePath = { MediaStore.Images.Media.DATA };
-                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
-                c.moveToFirst();
-                int columnIndex = c.getColumnIndex(filePath[0]);
-                String picturePath = c.getString(columnIndex);
-                c.close();
-                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
-                uploadImage.setImageBitmap(Bitmap.createScaledBitmap(thumbnail,uploadImage.getWidth(),uploadImage.getHeight(),false));
-            }
-        }
-    }
 
     @Override
     public void onClick(View view) {
