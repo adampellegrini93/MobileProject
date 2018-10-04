90198822779909bd32712a5e4ad0685033b2de59
jacques <farris.jacques@gmail.com>
2018-10-04 14:56:26 -0400

    update Jacques

--- app/src/main/java/android/project/AddContact.java
+++ app/src/main/java/android/project/AddContact.java
@@ -1,13 +1,21 @@
 package android.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AddContact extends AppCompatActivity {
+
+    private Toolbar myTool2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
     }
 }
+
